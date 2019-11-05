98646b1db65d434eed11a6c6a6b8e5efbe0a4bcf
Robert Stewart <rstewart414@gmail.com>
2019-11-05 15:48:59 -0500

    mvp + stretch

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import TeamMembers from "./components/TeamMembers.js";
+import Form from "./components/Form.js";
+
+import styles from "./styles.css";
+
+function App() {
+
+
+  const [teamMembers, setTeamMembers] = useState ([
+    {    
+      id:1,
+      name: "Meera Andersen",
+      email: "Meera@email.com",
+      role: "UX Designer"
+    }
+
+
+  ])
+
+  const addNewTeamMember = teamMember => {
+    const newTeamMember = {
+      id: Date.now(),
+      name: teamMember.name,
+      email: teamMember.email,
+      role: teamMember.role
+    };
+    setTeamMembers([...teamMembers, newTeamMember]);
+  };
+
+  const memberToEdit = teamMember => {
+
+  }
+
+
+
+  return (
+    <div className="App">
+      <h1>Add a New Team Member</h1>
+      <Form addNewTeamMember = {addNewTeamMember}/>
+      <TeamMembers teamMembers = {teamMembers}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TeamMembers.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const TeamMembers = props => {
+
+    return (
+    <div className = "card-container">
+        {props.teamMembers.map(teamMember => (
+            <div className ="team-member" key={teamMember.id}>
+                <h2>{teamMember.name}</h2>
+                <p>{teamMember.role}</p>
+                <p>{teamMember.email}</p>
+                <button>Edit</button>
+            </div>
+        ))}
+    </div>
+    );
+};
+
+export default TeamMembers; 
