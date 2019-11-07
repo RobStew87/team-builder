ce705f10ff768759537c779c3370d057cf0306cc
Robert Stewart <rstewart414@gmail.com>
2019-11-07 17:44:13 -0500

    fixed some stuff still having issue though but still working on it

---
+++ team-builder/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import TeamMembers from "./components/TeamMembers.js";
+import Form from "./components/Form.js/index.js";
+
+import styles from "./styles.css";
+
+function App() {
+
+
+  const [teamMembers, setTeamMembers] = useState ([
+    {    
+      id:1,
+      name: "Robert Stewart",
+      email: "rstewart414@gmail.com",
+      role: "web developer"
+    }
+
+
+  ])
+
+  const addNewTeamMember = teamMember => {
+    const newTeamMember = {
+      id: Date.now(),
+      name: teamMember.name,
+      email: teamMember.email,
+      role: teamMember.role
+    };
+    setTeamMembers([...teamMembers, newTeamMember]);
+  };
+
+  const memberToEdit = teamMember => {
+
+  }
+
+
+
+  return (
+    <div className="App">
+      <h1>Add a New Team Member</h1>
+      <Form addNewTeamMember = {addNewTeamMember}/>
+      <TeamMembers teamMembers = {teamMembers}/>
+    </div>
+  );
+}
+
+export default App;
